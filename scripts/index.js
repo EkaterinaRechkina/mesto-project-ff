2d2dc33fb74f42ca674e62d750c2c9e9b2ed96e1
Речкина Екатерина Викторовна <rechkina@miit.ru>
2024-07-16 12:30:35 +0300

    added createCard  and deleteCard functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardtemplate = document.querySelector('#card-template').content;
+const cardPlace = document.querySelector('.places__list');
 
+
+// @todo: DOM узлы
 // @todo: Функция создания карточки
+
+function createCard(el, deleteCard) {
+    const card = cardtemplate.querySelector('.card').cloneNode(true);
+    const cardDeleteBtn = card.querySelector('.card__delete-button')
+    let cardImg = card.querySelector('.card__image');
+    let cardTitle = card.querySelector('.card__title');
+    cardTitle.textContent = el.name;
+    cardImg.src = el.link;
+    cardDeleteBtn.addEventListener('click', deleteCard)
+    return card
+}
+
+// @todo: Вывести карточки на страницу
+
+initialCards.forEach(item => cardPlace.append(createCard(item, deleteCard)))
 
 // @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+function deleteCard() {
+    const cardDeleteBtn = document.querySelector('.card__delete-button')
+    const item = cardDeleteBtn.closest('.card');
+    item.remove()
+}
+
+
+
+
